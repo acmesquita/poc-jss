12bfd5531b2147c5b770c73c1a559d5ded0c0f63
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-01 13:00:36 -0300

    style: add border radius

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -3,6 +3,7 @@ import { makeStyles } from '../../hooks'
 export const useStyles = makeStyles({
   btn: {
     border: 'none',
+    borderRadius: 4,
     padding: '8px 16px',
     background: ({theme}) => theme.colorPrimary,
     cursor: 'pointer',
