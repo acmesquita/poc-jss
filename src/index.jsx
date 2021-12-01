76f65bd2d834c28dc5926acacd70e530a553abd8
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-01 11:50:15 -0300

    feat: add generateId to classes

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {JssProvider} from 'react-jss'
 import App from './App';
 
 
+const generateId = (rule, sheet) => `vindi-${rule.key}-${Math.floor(Math.random() * 5000)}`
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <JssProvider generateId={generateId}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </JssProvider>,
   document.getElementById('root')
 );
